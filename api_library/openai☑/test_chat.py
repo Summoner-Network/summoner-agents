286eaff1586d1a31e5176ecbe65ab00f42f33ab7
Remy Tuyeras <rtuyeras@gmail.com>
2025-10-13 18:40:11 -0400

    parsed outputs for the gpt agent

--- api_library/openai☑/test_chat.py
+++ api_library/openai☑/test_chat.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings("ignore", message=r".*supports OpenSSL.*LibreSSL.*")
 
-import os
+import os, json
 import asyncio
 import openai
 from openai import AsyncOpenAI
@@ -21,13 +21,14 @@ API_KEY = os.getenv("OPENAI_API_KEY")
 client =  AsyncOpenAI(api_key=API_KEY)
 model_ids = [m.id for m in openai.models.list().data]
 
-max_chat_output_tokens = 100
 max_chat_input_tokens = 100
-
+max_chat_output_tokens = 1000
 
 async def chat_agent(
         message: str = "Say this is a test", 
-        model_name: str = "gpt-4o-mini"
+        model_name: str = "gpt-4o-mini",
+        output_parsing: str = "text",
+        output_type = None
 ) -> ChatCompletion:
     if model_name not in model_ids:
         raise ValueError(
@@ -48,24 +49,64 @@ async def chat_agent(
     print(f"[chat] Estimated cost: ${est_cost:.6f}")
 
     if prompt_tokens < max_chat_input_tokens:
-        response = await client.chat.completions.create(
-                                messages=messages,
-                                model=model_name,
-                                max_completion_tokens=max_chat_output_tokens,
-                                )
-        pprint(response.usage.to_dict())
-        prompt_used  = response.usage.prompt_tokens
-        comp_used    = response.usage.completion_tokens
-        act_cost     = actual_chat_request_cost(model_name, prompt_used, comp_used)
-        print(f"[chat] Actual cost:    ${act_cost:.6f}")
+        if output_parsing == "text":
+            response = await client.chat.completions.create(
+                                    messages=messages,
+                                    model=model_name,
+                                    max_completion_tokens=max_chat_output_tokens
+                                    )
+            pprint(response.usage.to_dict())
+            prompt_used  = response.usage.prompt_tokens
+            comp_used    = response.usage.completion_tokens
+            act_cost     = actual_chat_request_cost(model_name, prompt_used, comp_used)
+            print(f"[chat] Actual cost:    ${act_cost:.6f}")
+            output = response.choices[0].message.content
+        elif output_parsing == "json":
+            response = await client.chat.completions.create(
+                                    messages=messages,
+                                    model=model_name,
+                                    max_completion_tokens=max_chat_output_tokens,
+                                    response_format={"type": "json_object"}
+                                    )
+            pprint(response.usage.to_dict())
+            prompt_used  = response.usage.prompt_tokens
+            comp_used    = response.usage.completion_tokens
+            act_cost     = actual_chat_request_cost(model_name, prompt_used, comp_used)
+            print(f"[chat] Actual cost:    ${act_cost:.6f}")
+            output = json.loads(response.choices[0].message.content)
+        elif output_parsing == "structured" and output_type is not None:
+            response = await client.responses.parse(
+                                    input=messages,
+                                    model=model_name, 
+                                    max_output_tokens=max_chat_output_tokens,
+                                    text_format=output_type
+                                    ) 
+            # output = response.output[0].content[0].text
+            output = response.output[0].content[0].parsed
+        else:
+            print("Output specification failed -- unable to send the request.")
     else:
-        print("Tokens exceeded -- requests was not sent.")
+        print("Tokens exceeded -- unable to send the request.")
     
-    return response
+    return output
 
 
-response = asyncio.run(chat_agent("What kind of communication protocol is missing for AI agents in a few words?"))
-print("Response:", response.choices[0].message.content)
+prompt = """What kind of communication protocol is missing for AI agents in a few words? 
+Structure your response in a json format using the structure {"number": (int) <number_of_features>, "elements": (list) [items,...]} where an item is a dictionary using the keys 'missing_feature' (str) and 'explanation' (str).                      
+"""
+print(prompt)
+
+from pydantic import BaseModel
+class Feature(BaseModel):
+    missing_feature: str
+    explanation: str
+
+class Output(BaseModel):
+    number: int
+    elements: list[Feature]
+
+output = asyncio.run(chat_agent(prompt, output_parsing="structured", output_type=Output))
+print("Response:", output)
 print()
 
 
